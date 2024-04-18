8dca88d347850ec4b74e7347d17cab52250534a6
yehonatanke <150778110+yehonatanke@users.noreply.github.com>
2024-04-18 17:45:20 +0200

    Add files via upload

---
+++ ex11/xv6-11/ps.c
@@ -0,0 +1,14 @@
+// User Program to Display Process Information (ps) 
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+
+int
+main(int argc, char *argv[]) {
+	cps130();
+	exit();
+}
+
